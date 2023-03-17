7509f7b033ed5838f831fb964691804f99dbfc50
wlankasper <asu.artur.smirnov@gmail.com>
2023-03-17 22:31:02 +0100

    beta header

---
+++ src/lib/components/header/Header.svelte
@@ -0,0 +1,25 @@
+<section id="header">
+    <div class="left">
+        <a href="/">
+            <h3 class="text">
+                CheeseBox
+            </h3>
+        </a>
+    </div>
+    <div class="right">
+        <h4 class="text text-accent">
+            Accedi
+        </h4>
+    </div>
+</section>
+
+<style>
+    section#header {
+        height: 12vh;
+
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-direction: row;
+    }
+</style>
